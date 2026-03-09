256575faea4bb50f5cf55ac0fe284d016d9f934a
Jonas L <jl.7804@gmail.com>
2026-03-09 16:52:04 +0100

    icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/main.scss'],
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/favicon/site.webmanifest' },
+      ],
+    },
+  },
   runtimeConfig: {
     weatherApiKey: process.env.WEATHER_API_KEY,
     public: {
